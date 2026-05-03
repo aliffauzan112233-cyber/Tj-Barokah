3c639c97647cc8143b173ee0b2c2c63d46d68bbf
Alif Fauzan <alif@email.com>
2026-05-03 09:17:31 +0700

    fix allowedDevOrigins for dev environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // @ts-ignore
+  allowedDevOrigins: ["192.168.30.11", "localhost:3000"],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["192.168.30.11", "localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
